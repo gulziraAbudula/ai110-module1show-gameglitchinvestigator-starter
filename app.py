845930032497c246e5504b74bd730143b74e9390
Gulzira Abdullah <gzabudula21@gmail.com>
2026-05-19 10:25:28 -0400

    added FIXME comments on issues and fixed the issues - range mismatch between sidebar and main page based on difficulty level and start new game status change to playing

--- app.py
+++ app.py
@@ -107,10 +107,9 @@ if "history" not in st.session_state:
 st.subheader("Make a guess")
 
 st.info(
-    f"Guess a number between 1 and 100. "
+    f"Guess a number between {low} and {high}. "
     f"Attempts left: {attempt_limit - st.session_state.attempts}"
 )
-
 with st.expander("Developer Debug Info"):
     st.write("Secret:", st.session_state.secret)
     st.write("Attempts:", st.session_state.attempts)
@@ -133,7 +132,8 @@ with col3:
 
 if new_game:
     st.session_state.attempts = 0
-    st.session_state.secret = random.randint(1, 100)
+    st.session_state.secret = random.randint(low, high)
+    st.session_state.status = "playing"
     st.success("New game started.")
     st.rerun()
 
